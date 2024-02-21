9ec5fce5cebad49b2c632e863e5b3c21571d3328
Emanuel Thomas <Man2Design7@gmail.com>
2024-02-20 20:43:05 -0500

    Third Commit

--- script.js
+++ script.js
@@ -1,45 +1,88 @@
-// // Create  a script that gathers data, processes it, and then outputs a consistent result as describe by a specification!
+// // // Create  a script that gathers data, processes it, and then outputs a consistent result as describe by a specification!
 
 
-const courseInfo = {
-    'name': 'How to Survive JS101',
-    'id': 22024
-};
+// const courseInfo = {
+//     'name': 'How to Survive ',
+//     'id': 101
+// };
 
-const assignmentGroup = {
-    'name': 'Assigment_One',
-    'id': 101,
-    'course_id' : 2000,
-    'group-weight': 10,
-    'assignments': [AssignmentInfo],
-}; 
+// const assignmentGroup = {
+//     'name': 'Survival Fundamentals',
+//     'id': 10124,
+//     'course_id' : 101,
+//     'group_weight': 20/100,
+//     'assignments': [
+//         {
+//             'id' : '01', 
+//             'name': 'Find Water!', 
+//             'due_at': '02.20.24',
+//             'points_possible': 20, 
+//         }, 
 
-const assigntmentInfo = [
-    assignmentObject = {
-        'name': 'name',
-        'id': 20,
-        'due_at': '12.16.24',
-        'points_possible': 100
-    }
-]; 
-
-const learnerSubmission = {
-    'learner_id': 604,
-    'assignment_id': 208,
-    'submission': {
-        'submitted_at': '08.12.25',
-        'score' : 50
-    }
-}; 
-
-
-
-
-
-
+//         {
+//             'id' : '02', 
+//             'name': 'Find Shelter!', 
+//             'due_at': '02.27.24',
+//             'points_possible': 20, 
+//         },
+        
+//         {
+//             'id' : '03', 
+//             'name': 'Yell for Help!', 
+//             'due_at': '03.05.24',
+//             'points_possible': 60, 
+//         }
+//     ]
+// }; 
 
 
 
-function getLearnerData(courseInfo, assignmentGroup, learnerSubmission,) {
+// const learnerSubmission = [
+//     {
+//         'learner_id': 604,
+//         'assignment_id': 10,
+//         'submission': {
+//             'submitted_at': '08.12.25',
+//             'score' : 95
+//         }
+//     }, 
+    
+//     {
+//         'learner_id': 604,
+//         'assignment_id': 20,
+//         'submission': {
+//             'submitted_at': '08.12.25',
+//             'score' : 85
+//         }
+//     },
+    
+//     {
+//         'learner_id': 604,
+//         'assignment_id': 30,
+//         'submission': {
+//             'submitted_at': '08.12.25',
+//             'score' : 75
+//         }
+//     }
+// ];
 
-}; 
+
+
+
+
+
+
+
+// console.log(cat);
+
+
+
+// // function getLearnerData(courseInfo, assignmentGroup, learnerSubmission,) {
+// //     return 
+// // }; 
+
+
+// // If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+
+
+console.log('cat');
